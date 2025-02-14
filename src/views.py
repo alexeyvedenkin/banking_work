4662b25c125b4eb01f99ad131bdba3bfb718ea46
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-02-15 04:15:39 +0500

    объявлены функции в src/views.py

---
+++ src/views.py
@@ -0,0 +1,20 @@
+import datetime
+import json
+import logging
+import pandas as pd
+
+from typing import Any, Dict, List, Optional
+
+from utils import read_excel
+
+
+def main_page():
+    """Возвращает JSON-ответ по настройкам
+    """
+    pass
+
+
+def events_page():
+    """Возвращает JSON-ответ по настройкам
+        """
+    pass
