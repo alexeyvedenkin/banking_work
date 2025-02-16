2c56d5decc082cd4a638bbcc7f6a7d7367dec9de
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-02-17 00:56:12 +0500

    реализована функция find_people_pass в модуле src/services.py

--- src/services.py
+++ src/services.py
@@ -1,49 +1,66 @@
-import datetime
 import json
 import logging
 import re
 
 import pandas as pd
 
-from typing import Any, Dict, List
+# from typing import Any, Dict, List
+#
+# from utils import read_excel
 
-from utils import read_excel
+logger = logging.getLogger("services")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('logs/services.log', "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+# def profitably_cacheback(data_dict):
+#     """Определяет наиболее выгодные категории для повышенного кэшбека
+#     """
+#     pass
+#
+#
+# def investment_bank(month: str, transactions: List[Dict[str, Any]], limit: int) -> float:
+#     """Возвращает сумму, которую удалось бы отложить в «Инвесткопилку».
+#     """
+#     pass
+#
+#
+# def easy_search(data_dict):
+#     """Возвращает JSON-ответ со всеми транзакциями, содержащими запрос в описании или категории
+#     """
+#     pass
+#
+#
+# def find_phone(df):
+#     """Возвращает JSON-файл со всеми транзакциями, содержащими в описании мобильные номера
+#     """
+#     pass
 
 
-def profitably_cacheback(data_dict):
-    """Определяет наиболее выгодные категории для повышенного кэшбека
-    """
-    pass
-
-
-def investment_bank(month: str, transactions: List[Dict[str, Any]], limit: int) -> float:
-    """Возвращает сумму, которую удалось бы отложить в «Инвесткопилку».
-    """
-    pass
-
-
-def easy_search(data_dict):
-    """Возвращает JSON-ответ со всеми транзакциями, содержащими запрос в описании или категории
-    """
-    pass
-
-
-def find_phone(df):
-    """Возвращает JSON-файл со всеми транзакциями, содержащими в описании мобильные номера
-    """
-    pass
-
-
-def find_people_pass(df):
+def find_people_pass(transactions: list[dict]) -> list[dict]:
     """Возвращает JSON-файл со всеми транзакциями, которые относятся к переводам физлицам
     """
-    filtered_data = df[df['Описание'].str.contains('Перевод')]
-    result_list = filtered_data.sort_values(by='Дата операции', ascending=True)
-    result_list.to_json('people_pass.json', indent=4, orient='records', force_ascii=False)
+    pattern = re.compile(r'\w+\s\w\.')
+    logger.info("Определен шаблон для поиска в описании операций")
+    result_list = []
+    logger.info("Начало обработки данных")
+    try:
+        for transaction in transactions:
+            if transaction.get('Категория') == 'Переводы' and re.search(pattern, transaction.get('Описание')):
+                logger.debug("Найден перевод")
+                result_list.append(transaction)
+    except Exception as e:
+        return []
     return result_list
 
 
 if __name__ == '__main__':
     df = pd.read_excel('operations.xlsx')
-    result = find_people_pass(df)
-    print(find_people_pass(df), sep='\n')
+    transactions = df.to_dict(orient="records")
+    result = find_people_pass(transactions)
+    print(*result, sep='\n')
+    print(len(result))
+    with open('people_pass.json', 'w', encoding='utf-8') as json_file:
+        json.dump(result, json_file, indent=4, ensure_ascii=False)
