d947a96b2ec9764c23c23e750c94d19e0b913e53
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-02-19 02:55:33 +0500

    fixed spending_by_period in src/views.py

--- src/views.py
+++ src/views.py
@@ -28,6 +28,7 @@ def spending_by_period(df, work_date: str, type_of_period: str='W') -> DataFrame
     # Определяем период для обработки исходя из заданной даты
     start = utils.get_start_for_period(work_date, type_of_period)
     work_date = datetime.datetime.strptime(work_date, "%d.%m.%Y %H:%M:%S")
+    print()
     print(f'Период выборки: с {start} до {work_date}')
     print()
 
@@ -40,6 +41,12 @@ def spending_by_period(df, work_date: str, type_of_period: str='W') -> DataFrame
         & (df['Сумма операции'] < 0)
         ]
 
+    # Определяем общую сумму платежей за период
+    total_payments = int(filtered_df['Сумма операции'].apply(lambda x: abs(x) if x < 0 else 0).sum())
+    print(f'Общая сумма платежей за период выборки: {total_payments} руб.')
+    print()
+
+
     # Группируем расходы по категориям
     spending_by_categories = filtered_df.groupby('Категория', observed=False)
 
@@ -49,18 +56,18 @@ def spending_by_period(df, work_date: str, type_of_period: str='W') -> DataFrame
     # Сортируем результат группировки по убыванию
     sorted_result = result.sort_values(ascending=False)
 
-    # Выделяем основные категории (7 позиций)
-    top_7 = sorted_result.head(7)
+    # Выделяем основные категории (7 позиций), округляем сумму по категориям до целых чисел
+    top_7 = sorted_result.head(7).astype(int)
 
-    # Суммируем категории, не попавшие в top_7 в категорию "Остальное"
-    others = pd.Series({'Остальное': sorted_result.iloc[7:].sum()})
+    # Суммируем категории, не попавшие в top_7, в категорию "Остальное", округляем сумму до целого числа
+    others = pd.Series({'Остальное': sorted_result.iloc[7:].sum().astype(int)})
 
     # Объединяем в итоговый датафрейм
-    total = pd.concat([top_7, others])
+    total_df = pd.concat([top_7, others])
 
-    return total
+    return total_df
 
 
 if __name__ == '__main__':
     df = pd.read_excel('operations.xlsx')
-    print(spending_by_period(df, '31.12.2021 23:59:59', 'M'))
+    print(spending_by_period(df, '31.12.2021 23:59:59', 'Y'))
