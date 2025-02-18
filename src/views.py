faf5e9ef66255b4a7e3e6f30505b6c01349cdf53
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-02-19 01:49:14 +0500

    реализована функция spending_by_period в src/views.py

--- src/views.py
+++ src/views.py
@@ -1,11 +1,13 @@
 import datetime
-import json
-import logging
+# import json
+# import logging
 import pandas as pd
+#
+# from typing import Any, Dict, List, Optional
+#
+from pandas import DataFrame
 
-from typing import Any, Dict, List, Optional
-
-from utils import read_excel
+from src import utils
 
 
 def main_page():
@@ -16,5 +18,49 @@ def main_page():
 
 def events_page():
     """Возвращает JSON-ответ по настройкам
-        """
+    """
     pass
+
+
+def spending_by_period(df, work_date: str, type_of_period: str='W') -> DataFrame:
+    """Возвращает общую сумму расходов за период
+    """
+    # Определяем период для обработки исходя из заданной даты
+    start = utils.get_start_for_period(work_date, type_of_period)
+    work_date = datetime.datetime.strptime(work_date, "%d.%m.%Y %H:%M:%S")
+    print(f'Период выборки: с {start} до {work_date}')
+    print()
+
+    # Преобразовываем столбец "Дата операции" из формата str в столбец с объектами datetime
+    df['Дата операции'] = pd.to_datetime(df['Дата операции'], dayfirst=True)
+
+    # Фильтруем исходный датафрейм по заданному периоду и отрицательной сумме операции (платежи)
+    filtered_df = df[(start <= df['Дата операции'])
+        & (df['Дата операции'] <= work_date)
+        & (df['Сумма операции'] < 0)
+        ]
+
+    # Группируем расходы по категориям
+    spending_by_categories = filtered_df.groupby('Категория', observed=False)
+
+    # Определяем сумму расходов по категориям в заданном периоде
+    result = spending_by_categories['Сумма операции'].apply(lambda x: x[x < 0].abs().sum())
+
+    # Сортируем результат группировки по убыванию
+    sorted_result = result.sort_values(ascending=False)
+
+    # Выделяем основные категории (7 позиций)
+    top_7 = sorted_result.head(7)
+
+    # Суммируем категории, не попавшие в top_7 в категорию "Остальное"
+    others = pd.Series({'Остальное': sorted_result.iloc[7:].sum()})
+
+    # Объединяем в итоговый датафрейм
+    total = pd.concat([top_7, others])
+
+    return total
+
+
+if __name__ == '__main__':
+    df = pd.read_excel('operations.xlsx')
+    print(spending_by_period(df, '31.12.2021 23:59:59', 'M'))
