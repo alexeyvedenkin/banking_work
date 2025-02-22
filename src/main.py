f1b448a93614a31bede3b35b9dcc302773461493
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-02-22 19:05:59 +0500

    modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
-# import os
+import os
 import pandas as pd
-from src import reports, services
+from src import reports, services, utils, views
+from config import DATA_DIR, LOGS_DIR, RESULT_DIR
+import logging
+
+logger = logging.getLogger("main")
+logger.setLevel(logging.DEBUG)
+log_file_path = os.path.join(LOGS_DIR, 'main.log')
+file_handler = logging.FileHandler(log_file_path, "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
-df = pd.read_excel('operations.xlsx')
+work_date = "18.02.2025 22:54:00"
+operations_path = os.path.join(DATA_DIR, 'operations.xlsx')
+df = pd.read_excel(operations_path)
 data = df.to_dict('records')
 
 
@@ -12,7 +24,8 @@ print('Результат работы функции find_people_pass:')
 print()
 print('Формат результирующих данных (выведено первые 5 записей c переводами физическим лицам):')
 services.find_people_pass(data)
-print('Результат записан в файл src/people_pass.json')
+people_pass_path = os.path.join(RESULT_DIR, 'people_pass.json')
+print(f'Результат записан в файл {str(people_pass_path)}')
 print()
 
 print()
