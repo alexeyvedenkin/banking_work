133ed8d1d55e2ebc115a416d8e5a436152eb62b9
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-02-25 23:38:58 +0500

    modified:   tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,20 +9,12 @@ def test_date() -> str:
     """
     Тестовые данные для применения datetime
     """
-    test_date = datetime.datetime.strptime('31.03.2021 23:59:59', "%d.%m.%Y %H:%M:%S")
+    test_date = datetime.strptime('31.03.2021 23:59:59', "%d.%m.%Y %H:%M:%S")
 
     return test_date
 
 
-# def test_datetime() -> datetime.datetime:
-#     """
-#     Тестовые данные для применения datetime
-#     """
-#     test_dat_time = 2025-12-18 22:54:00
-#
-#     return test_dat_time
-
-
+@pytest.fixture
 def test_days_translation():
     return {
         'Monday': 'Понедельник',
@@ -34,4 +26,4 @@ def test_days_translation():
         'Sunday': 'Воскресенье'
     }
 
-# assert test_days_translation['Monday'] == 'Понедельник'
+
