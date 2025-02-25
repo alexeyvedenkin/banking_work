7b95668b737a918371412b3055bec564d6094eca
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-02-25 23:37:44 +0500

    modified:   src/decorator.py

--- src/decorator.py
+++ src/decorator.py
@@ -6,7 +6,7 @@ from functools import wraps
 import pandas as pd
 
 from config import DATA_DIR, LOGS_DIR, RESULT_DIR
-from src.reports import spending_by_weekday
+
 
 logger = logging.getLogger("decorator")
 logger.setLevel(logging.DEBUG)
@@ -17,36 +17,38 @@ file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
 
-def to_json_file(filename):
+def to_json_file(filename='reports.json'):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             logger.info('Результат работы функции записан в переменную result')
-            result = func(*args, **kwargs)
+            result: pd.DataFrame = func(*args, **kwargs)
 
             # Преобразуем объект Series в словарь вручную
             logger.info('Начало преобразования объекта Series в словарь')
-            series_dict = {index: round(value, 2) for index, value in result.items()}
+            # series_dict = {index: round(value, 2) for index, value in result.items()}
 
             # Выгружаем словарь в JSON-файл
             logger.info('Начало выгрузки отчета')
-            reports_path = os.path.join(RESULT_DIR, 'reports.json')
-            with open(reports_path, 'w', encoding='utf-8') as f:
-                json.dump(series_dict, f, indent=4, ensure_ascii=False)
+            reports_path = os.path.join(RESULT_DIR, filename)
+            result.to_json(path_or_buf=reports_path, indent=4, force_ascii=False)
+            # with open(reports_path, 'w', encoding='utf-8') as f:
+            #     json.dump(series_dict, f, indent=4, ensure_ascii=False)
             logger.info('Окончание выгрузки отчета')
             return result
         return wrapper
     return decorator
 
 
-# Создаем функцию, которая возвращает объект Series
-@to_json_file('series.json')
-def create_series():
-    logger.info('Формирование функции для обработки')
-    operations_path = os.path.join(DATA_DIR, 'operations.xlsx')
-    df = pd.read_excel(operations_path)
-    result = spending_by_weekday(df, '31.03.2021 23:59:59')
-    return result
+
+# # Создаем функцию, которая возвращает объект Series
+# @to_json_file()
+# def create_series():
+#     logger.info('Формирование функции для обработки')
+#     operations_path = os.path.join(DATA_DIR, 'operations.xlsx')
+#     df = pd.read_excel(operations_path)
+#     result = spending_by_weekday(df, '31.03.2021 23:59:59')
+#     return result
 
 # Вызываем функцию
 # if __name__ == '__main__':
